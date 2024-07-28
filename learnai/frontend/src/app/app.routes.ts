041a9bd073ca8a3e5894ef3f430962797a26676e
Vedat <vedatalpgktp@gmail.com>
2024-07-27 22:58:57 -0400

    added basic login page structure

--- learnai/frontend/src/app/app.routes.ts
+++ learnai/frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 export const routes: Routes = [
-  {path: '', component: MainPageComponent}
+  {path: '', component: LoginComponent},
+  {path: 'main', component: MainPageComponent}
 ];
