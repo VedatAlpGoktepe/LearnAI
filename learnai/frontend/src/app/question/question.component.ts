5ba558f24ae2d105fd071e6ec557001526996d1b
Vedat <vedatalpgktp@gmail.com>
2024-07-29 00:34:23 -0400

    implemented question functionality

--- learnai/frontend/src/app/question/question.component.ts
+++ learnai/frontend/src/app/question/question.component.ts
@@ -1,15 +1,20 @@
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-question',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, MatButtonModule, NgClass],
   templateUrl: './question.component.html',
   styleUrl: './question.component.scss'
 })
 export class QuestionComponent {
   @Input() question: string = '';
-  @Input() answer: string = '';
-  @Input() options: string[] = [];
+  @Input() options: any[] = [];
+  selectedId: number = -1;
+
+  selectAnswer(index: any) {
+    this.selectedId = index;
+  }
 }
